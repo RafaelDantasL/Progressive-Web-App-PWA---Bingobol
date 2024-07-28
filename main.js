d4579f8e60984b2d967e0be5b9d37d73b8472472
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2024-07-28 15:48:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'acertos-jb-cache-v1';
 const urlsToCache = [
   '/',
-  'https://bingobol.blogspot.com/p/main.html',
+  'https://bingobol.blogspot.com',
   '/styles/main.css',  // Adicione aqui todos os recursos que deseja armazenar em cache
   '/scripts/main.js',  // Adicione aqui todos os recursos que deseja armazenar em cache
   '/icon512_maskable.png',
