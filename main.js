28855bea8b74fe4a942fd5fa11709669419a34bc
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2024-07-28 15:39:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,50 @@
-self.addEventListener('install', function(event) {
+// Nome do cache e arquivos a serem armazenados
+const CACHE_NAME = 'acertos-jb-cache-v1';
+const urlsToCache = [
+  '/',
+  'https://bingobol.blogspot.com/p/main.html',
+  '/styles/main.css',  // Adicione aqui todos os recursos que deseja armazenar em cache
+  '/scripts/main.js',  // Adicione aqui todos os recursos que deseja armazenar em cache
+  '/icon512_maskable.png',
+  '/icon512_rounded.png'
+];
+
+// Evento de instalação do service worker
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/script.js',
-        '/images/icons/icon-192x192.jpeg',
-        '/images/icons/icon-512x512.jpeg'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+// Evento de busca
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;  // Retorna do cache se disponível
+        }
+        return fetch(event.request);  // Faz uma requisição à rede se não estiver no cache
+      })
+  );
+});
+
+// Evento de ativação
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);  // Remove caches antigos
+          }
+        })
+      );
     })
   );
 });
