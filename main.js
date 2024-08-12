e57ae72ee94cb3d0b4356435b81517121122ec67
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2024-08-12 14:00:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 // Nome do cache e arquivos a serem armazenados
 const CACHE_NAME = 'acertos-Online-cache-v1';
+const OFFLINE_URL = '/offline.html';  // Caminho da página offline personalizada
 const urlsToCache = [
   '/',
   'https://www.acertosonline.com/p/app.html',
   '/styles/main.css',  // Adicione aqui todos os recursos que deseja armazenar em cache
   '/scripts/main.js',  // Adicione aqui todos os recursos que deseja armazenar em cache
   '/icon512_maskable.png',
-  '/icon512_rounded.png'
+  '/icon512_rounded.png',
+  OFFLINE_URL  // Inclua a página offline no cache
 ];
 
 // Evento de instalação do service worker
@@ -28,7 +30,12 @@ self.addEventListener('fetch', event => {
         if (response) {
           return response;  // Retorna do cache se disponível
         }
-        return fetch(event.request);  // Faz uma requisição à rede se não estiver no cache
+        return fetch(event.request).catch(() => {
+          // Se a requisição falhar (offline), retorna a página offline
+          if (event.request.mode === 'navigate') {
+            return caches.match(OFFLINE_URL);
+          }
+        });
       })
   );
 });
