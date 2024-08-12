70fb90a180e2e9b6a6b547a521307fb6a226cf86
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2024-08-12 14:34:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 // Nome do cache e arquivos a serem armazenados
 const CACHE_NAME = 'acertos-Online-cache-v1';
-const OFFLINE_URL = 'https://rafaeldantasl.github.io/Progressive-Web-App-PWA---Bingobol/offline.html';  // Caminho da página offline personalizada
 const urlsToCache = [
   '/',
   'https://www.acertosonline.com/p/app.html',
-  '/styles/main.css',  // Adicione aqui todos os recursos que deseja armazenar em cache
-  '/scripts/main.js',  // Adicione aqui todos os recursos que deseja armazenar em cache
+  '/styles/main.css',
+  '/scripts/main.js',
   '/icon512_maskable.png',
   '/icon512_rounded.png',
-  OFFLINE_URL  // Inclua a página offline no cache
+  '/offline.html'  // Adicione a página offline aqui
 ];
 
 // Evento de instalação do service worker
@@ -31,10 +30,7 @@ self.addEventListener('fetch', event => {
           return response;  // Retorna do cache se disponível
         }
         return fetch(event.request).catch(() => {
-          // Se a requisição falhar (offline), retorna a página offline
-          if (event.request.mode === 'navigate') {
-            return caches.match(OFFLINE_URL);
-          }
+          return caches.match('/offline.html');  // Retorna a página offline se a rede falhar
         });
       })
   );
