1fbf4ac2e66e6fe64c72122328cea5c3947710e8
Rafael <157317208+RafaelDantasL@users.noreply.github.com>
2024-08-12 13:54:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 // Nome do cache e arquivos a serem armazenados
-const CACHE_NAME = 'acertos-online-cache-v1';
+const CACHE_NAME = 'acertos-Online-cache-v1';
 const urlsToCache = [
   '/',
   'https://www.acertosonline.com/p/app.html',
-  '/styles/main.css',
-  '/scripts/main.js',
+  '/styles/main.css',  // Adicione aqui todos os recursos que deseja armazenar em cache
+  '/scripts/main.js',  // Adicione aqui todos os recursos que deseja armazenar em cache
   '/icon512_maskable.png',
-  '/icon512_rounded.png',
-  '/offline.html'  // Certifique-se de que este caminho seja correto
+  '/icon512_rounded.png'
 ];
 
 // Evento de instalação do service worker
@@ -26,10 +25,10 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        return response || fetch(event.request).catch(() => {
-          // Se a rede falhar, retorna o offline.html do cache
-          return caches.match('/offline.html');
-        });
+        if (response) {
+          return response;  // Retorna do cache se disponível
+        }
+        return fetch(event.request);  // Faz uma requisição à rede se não estiver no cache
       })
   );
 });
